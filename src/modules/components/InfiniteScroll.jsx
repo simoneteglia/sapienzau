fc7be3eea39ddd2bcf2f5572d7179f716498aac0
Simone Teglia <simone.teglia9@gmail.com>
2026-02-02 21:40:38 +0100

    team logos in infinite scroll

--- src/modules/components/InfiniteScroll.jsx
+++ src/modules/components/InfiniteScroll.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
-import twitter from "../../assets/logos/twitter.png";
-import linkedin from "../../assets/logos/linkedin.png";
-import youtube from "../../assets/logos/youtube.png";
+import sec from "../../assets/logos/team_sec.png";
+import cemm from "../../assets/logos/team_cemm.png";
+import dex from "../../assets/logos/team_dex.png";
+import ers from "../../assets/logos/team_ers.png";
+import hra from "../../assets/logos/team_hra.png";
+import it from "../../assets/logos/team_it.png";
+import la from "../../assets/logos/team_la.png";
+import pem from "../../assets/logos/team_pem.png";
 
 export default function InfiniteScroll() {
   const logos = [
-    { src: twitter, alt: "Twitter Logo" },
-    { src: linkedin, alt: "LinkedIn Logo" },
-    { src: youtube, alt: "YouTube Logo" },
-    // You can easily add more here:
-    // { src: logo4, alt: "Logo 4" },
+    { src: sec, alt: "SEC Logo" },
+    { src: cemm, alt: "CEMM Logo" },
+    { src: dex, alt: "DEX Logo" },
+    { src: ers, alt: "ERS Logo" },
+    { src: hra, alt: "HRA Logo" },
+    { src: it, alt: "IT Logo" },
+    { src: la, alt: "LA Logo" },
+    { src: pem, alt: "PEM Logo" },
   ];
 
   // Logic: We repeat the list inside the <ul> to fill horizontal space,
@@ -19,21 +27,21 @@ export default function InfiniteScroll() {
   const displayLogos = Array(LOGO_REPETITIONS).fill(logos).flat();
 
   const ulClassName =
-    "flex flex-nowrap items-center flex-shrink-0 [&_li]:mx-4 animate-infinite-scroll";
+    "flex flex-nowrap items-center flex-shrink-0 [&_li]:mx-3 animate-infinite-scroll";
 
   return (
     <div className="w-screen inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_100px,_black_calc(100%-100px),transparent_100%)]">
       <ul className={ulClassName}>
         {displayLogos.map((logo, index) => (
           <li key={`primary-${index}`}>
-            <img src={logo.src} alt={logo.alt} className="max-w-none" />
+            <img src={logo.src} alt={logo.alt} className="max-w-[60px]" />
           </li>
         ))}
       </ul>
       <ul className={ulClassName} aria-hidden="true">
         {displayLogos.map((logo, index) => (
           <li key={`secondary-${index}`}>
-            <img src={logo.src} alt={logo.alt} className="max-w-none" />
+            <img src={logo.src} alt={logo.alt} className="max-w-[60px]" />
           </li>
         ))}
       </ul>
