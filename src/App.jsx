d6c2684daa13be9793174d08ba9081a17e78ff05
Stefano D'Agnese <80704456+stedagne@users.noreply.github.com>
2026-05-05 11:13:03 +0200

    shop hidden due to supplier change

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/shop",
-        element: <Shop />,
+        element: <WIP />
       },
       {
         path: "/shop/:item",
