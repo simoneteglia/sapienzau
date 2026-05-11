34fdd41415e3b145c2477a6410d900ce16b765e2
Leonardo <leonardomarasca@gmail.com>
2026-05-11 16:23:28 +0200

    pagina shop aggiornata e visibile

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/shop",
-        element: <WIP />
+        element: <Shop />
       },
       {
         path: "/shop/:item",
