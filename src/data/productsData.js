afea3396da3c8e5c81cfb03a17a428f4445624cf
Leonardo <leonardomarasca@gmail.com>
2026-05-12 19:35:55 +0200

    fine nuovo shop

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -3,7 +3,7 @@ import { color } from "three/src/nodes/tsl/TSLCore.js";
 export const productsData = [
   {
     id: 1,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572277/felpa-unisex/black/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572277/felpa-unisex/black/500/",
     imageUrl2: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Back/00572277/felpa-unisex/black/100/",
     title: "Felpa Team Logos",
     description: "Felpa unisex",
@@ -13,7 +13,7 @@ export const productsData = [
   },
   {
     id: 2,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572251/maglietta-unisex/black/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572251/maglietta-unisex/black/500/",
     imageUrl2: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Back/00572251/maglietta-unisex/black/100/",
     title: "Logo Pattern T-Shirt",
     description: "T-shirt unisex ",
@@ -34,7 +34,7 @@ export const productsData = [
   {
     id: 4,
     imageUrl2: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Back/00572250/maglietta-unisex/white/100/",
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572250/maglietta-unisex/white/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572250/maglietta-unisex/white/500/",
     title: "Logo Pattern T-Shirt",
     description: "T-shirt Unisex",
     price: "17.00",
@@ -43,7 +43,7 @@ export const productsData = [
   },
   {
     id: 5,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572280/caps-pre/black/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572280/caps-pre/black/500/",
     imageUrl2: "https://d29gv5mnjp8nf8.cloudfront.net/api/watermark/00572280/Front.png/little",
     title: "TEDxSapienzaU Baseball Hat",
     description: "Baseball cap premium",
@@ -53,7 +53,7 @@ export const productsData = [
   },
   {
     id: 6,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572283/caps-pre/black/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572283/caps-pre/black/500/",
     title: "Ideas Change Everything Baseball Hat",
     description: "Baseball cap premium",
     price: "13.00",
@@ -62,7 +62,7 @@ export const productsData = [
   },
   {
     id: 7,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572292/caps-pre/white/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572292/caps-pre/white/500/",
     title: "Ideas Change Everything Baseball hat",
     description: "Baseball cap premium",
     price: "13.00",
@@ -71,7 +71,7 @@ export const productsData = [
   },
   {
     id: 8,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572304/caps-pre/white/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572304/caps-pre/white/500/",
     title: "Logo pattern baseball Cap",
     description: "Baseball Cap premium",
     price: "13.00",
@@ -80,7 +80,7 @@ export const productsData = [
   },
   {
     id: 9,
-    imageUrl: "https://teetalyimmages.blob.core.windows.net/thumbs/1259804_IMG_19-35-19-3519134212737195138127_600.png",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572307/mug-twotone/black/500/",
     title: "Tazza logo pattern",
     description: "Mug two color",
     price: "13.00",
@@ -98,7 +98,7 @@ export const productsData = [
   },
   {
     id: 11,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572315/borsa/white/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572315/borsa/white/500/",
     title: "Tote Bag TEDxSapienzaU",
     description: "Shopping bag in cotone canvas",
     price: "17.50",
@@ -108,7 +108,7 @@ export const productsData = [
 
   {
     id: 14,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572319/borsa/black/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572319/borsa/black/500/",
     title: "Tote Bag TEDxSapienzaU",
     description: "Shopping bag in cotone canvas",
     price: "13.0",
@@ -118,7 +118,7 @@ export const productsData = [
 
   {
     id: 12,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572327/pins/nd/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572327/pins/nd/500/",
    
     title: "Logo pattern pin",
     description: "Spille",
@@ -128,11 +128,114 @@ export const productsData = [
   },
   {
     id: 13,
-    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572328/pins/nd/100/",
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00572328/pins/nd/500/",
     title: "Team logos pin",
     description: "Spille",
     price: "13.00",
     link: "https://sapienzau-shop.hoplix.shop/1263803-pins-team-logos-pin-62900.htm",
     color: "#fff"
   },
+
+ {
+    id: 15,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573388/note/black/500/",
+    title: "Quaderno SapienzaU Teams",
+    description: "Quaderno",
+    price: "14.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265232-note-quaderno-sapienzau-teams-62900.htm",
+    color: "#fff"
+  },
+
+   {
+    id: 16,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573387/kchain-sq/nd/500/",
+    title: "Portachiavi TedXSapienzaU Logo",
+    description: "Portachiavi",
+    price: "12.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265231-kchain-sq-portachiavi-tedxsapienzau-62900.htm",
+    color: "#fff"
+  },
+
+   {
+    id: 17,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573386/kchain-sq/nd/500/",
+    title: "Portachiavi Team logos",
+    description: "Portachiavi",
+    price: "12.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265230-kchain-sq-portachiavi-team-logos-62900.htm",
+    color: "#fff"
+  },
+
+  {
+    id: 18,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573366/kchain-sq/nd/500/",
+    title: "Portachiavi Ideas change everything",
+    description: "Portachiavi",
+    price: "12.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265198-kchain-sq-portachiavi-ideas-change-everything-62900.htm",
+    color: "#fff"
+  },
+
+  {
+    id: 19,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573363/kchain-sq/nd/500/",
+    title: "Portachiavi Logo pattern",
+    description: "Portachiavi",
+    price: "12.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265195-kchain-sq-portachiavi-logo-pattern-62900.htm",
+    color: "#fff"
+  },
+
+  {
+    id: 20,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573361/sticker-ro-10-kit6/nd/500/",
+    title: "Adesivi ideas change everything",
+    description: "Adesivi x 6",
+    price: "18.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265193-sticker-ro-10-kit6-adesivi-ideas-change-everything-62900.htm",
+    color: "#fff"
+  },
+
+{
+    id: 20,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573361/sticker-ro-10-kit6/nd/500/",
+    title: "Adesivi ideas change everything",
+    description: "Adesivi x 6",
+    price: "18.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265193-sticker-ro-10-kit6-adesivi-ideas-change-everything-62900.htm",
+    color: "#fff"
+  },
+
+ 
+    {
+    id: 22,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573359/sticker-ro-10-kit6/nd/500/",
+    title: "Adesivi logo pattern",
+    description: "Adesivi x 6",
+    price: "18.90",
+    link: "https://sapienzau-shop.hoplix.shop/1265191-sticker-ro-10-kit6-adesivi-logo-pattern-62900.htm",
+    color: "#fff"
+  },
+
+
+    {
+    id: 23,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573390/poster-30x45/glossy/500/",
+    title: "Poster logo pattern",
+    description: "Poster",
+    price: "12.50",
+    link: "https://sapienzau-shop.hoplix.shop/1265241-poster-30x45-poster-logo-pattern-62900.htm",
+    color: "#fff"
+  },
+
+   {
+    id: 24,
+    imageUrl: "https://d29gv5mnjp8nf8.cloudfront.net/showimaged/Front/00573393/poster-30x45/glossy/500/",
+    title: "Poster teams",
+    description: "Poster",
+    price: "12.50",
+    link: "https://sapienzau-shop.hoplix.shop/1265245-poster-30x45-poster-teams-62900.htm",
+    color: "#fff"
+  },
+
 ];
